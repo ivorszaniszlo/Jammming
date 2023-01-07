6619db5b7887e183744d67d1e72bca736f8df53e
Ivor Szaniszló <szaniszlo.ivor@gmail.com>
2023-01-07 16:57:21 +0100

    FIX: imports

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -13,7 +13,7 @@ class Track extends React.Component {
       return (
         <div className="Track">
             <div className="Track-information">
-                <h3><!-- track name will go here --></h3>
+               <h3><!-- track name will go here --></h3>
                 <p><!-- track artist will go here--> | <!-- track album will go here --></p>
             </div>
             <button className="Track-action"><!-- + or - will go here --></button>
